cc74792b99ec1f89ed4a1172db2cc2ba608929fb
sushil <gpsushil@gmail.com>
2023-06-24 15:26:58 +0530

    tested customer and product

--- backend/app/controllers/customersCtlr.js
+++ backend/app/controllers/customersCtlr.js
@@ -4,43 +4,44 @@ const customersCtlr={}
 
 customersCtlr.create=async(req,res)=>{
     try{
-         const body=req.body
-         const customer=new Customer(body)
-         const saveObj= await customer.save()
-         res.json(saveObj)
+         const body = req.body
+         const customerObj = new Customer(body)
+         const customer = await customerObj.save()
+         res.json(customer)
     }catch(e){
          res.json(e)
     }
 }
 
-customersCtlr.list=async(req,res)=>{
+customersCtlr.list = async(req,res)=>{
     try{
-        const customer=await Customer.find()
+        const customer = await Customer.find()
         res.json(customer)
     }catch(e){
         res.json(e)
     }
 }
 
-customersCtlr.update=async(req,res)=>{
+customersCtlr.update = async(req,res)=>{
     try{
-        const id=req.params.id
-        const body=req.body
-        const customer=await Customer.findByIdAndUpdate(id,body,{new:true,runValidators:true})
+        const id = req.params.id
+        const body = req.body
+        const customer = await Customer.findByIdAndUpdate(id,body,{new:true,runValidators:true})
         res.json(customer)
     }catch(e){
         res.json(e)
     }
 }
 
-customersCtlr.destroy=async(req,res)=>{
+customersCtlr.destroy = async(req,res)=>{
     try{
-         const id=req.params.id
-         const obj=await Customer.findByIdAndDelete(id)
+         const id = req.params.id
+         const obj = await Customer.findByIdAndDelete(id)
          res.json(obj)
     }catch(e){
         res.json(e)
     }
 }
 
-module.exports=customersCtlr
+
+module.exports = customersCtlr
