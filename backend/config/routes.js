d2cd4d6f389f7b7bce84553ec6a5f7a8dca97b2a
Nishan Bangera <rakshukanva@gmail.com>
2023-06-24 12:52:01 +0530

    Customers model and controllers are added with routes

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const usersCtlr = require('../app/controllers/usersCtlr')
+const customersCtlr=require('../app/controllers/customersCtlr')
 const authenticateUser = require('../app/middlewares/authenticateUser')
 
 router.post('/api/users/register', usersCtlr.account)
 router.post('/api/users/login', usersCtlr.login)
 router.get('/api/users/account', authenticateUser, usersCtlr.account)
+
+router.post('/api/customers',customersCtlr.create)
+router.get('/api/customers',customersCtlr.list)
+router.put('/api/customers:id',customersCtlr.update)
+router.delete('/api/customers:id',customersCtlr.destroy)
 
 module.exports = router
 
