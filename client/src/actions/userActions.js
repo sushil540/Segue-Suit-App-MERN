bfe56a5299f2156ad9f34d0d1e97ac5e5277986c
Nishan Bangera <rakshukanva@gmail.com>
2023-06-30 11:58:12 +0530

    user errors are stored

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,7 @@
 import axios from "../config/axios"
 
+export const LOGIN_ERRORS = 'LOGIN_ERRORS'
+
 export const startRegisterUser = (formData)=>{
     return (dispatch) =>{
         (async ()=>{
@@ -13,14 +15,26 @@ export const startRegisterUser = (formData)=>{
     }
 }
 
+
+
+const setErrors=(user)=>{
+    console.log('user',user)
+    return{
+         type:LOGIN_ERRORS,
+         payload:user
+    }
+}
 export const startLoginUser = (formData) =>{
     return (dispatch) =>{
         (async ()=>{
             try{
             const response = await axios.post('/api/users/login',formData)
-            if(!response.data?.errors){
+            console.log(response)
+            if(!response.data?.error){
                 localStorage.setItem('token',response.data.token)
                
+            }else{
+                dispatch(setErrors(response.data.error))
             }
             }catch(e){
               alert(e)
