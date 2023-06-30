9dc2abf36dfcef3f3fe21bd220c0e5e11ac94440
sushil <gpsushil@gmail.com>
2023-06-30 14:11:31 +0530

    modified Register component

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,16 +1,23 @@
 import axios from "../config/axios"
+export const SET_ERRORS = 'LOGIN_ERRORS'
 
-export const LOGIN_ERRORS = 'LOGIN_ERRORS'
+export const setErrors=(user)=>{
+    return{
+         type:SET_ERRORS,
+         payload:user
+    }
+}
 
-export const startRegisterUser = (formData)=>{
+export const startRegisterUser = (formData, props)=>{
     return (dispatch) =>{
         (async ()=>{
             try{
                 const response = await axios.post('/api/users/register',formData)
                 console.log('response',response.data)
                 if(response.data.hasOwnProperty('errors')){
-                        // response.data.errors.message
-                    // dispatch()
+                    dispatch(setErrors(response.data.errors.email.message.split(". ")[0]))
+                }else{
+                    props.history.push('/login')
                 }
             }catch(e){
                 alert(e)
@@ -19,15 +26,6 @@ export const startRegisterUser = (formData)=>{
     }
 }
 
-
-
-const setErrors=(user)=>{
-    console.log('user',user)
-    return{
-         type:LOGIN_ERRORS,
-         payload:user
-    }
-}
 export const startLoginUser = (formData) =>{
     return (dispatch) =>{
         (async ()=>{
